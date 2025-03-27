4770fdd0ba927660e1b6895d418db80f98b964f0
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-27 20:32:04 -0300

    feat(models): add PessoaJuridica entity class
    Introduce the PessoaJuridica entity class to represent legal entities in the database. This class includes fields such as id, faturamento, idade, nome_fantasia, celular, email_corporativo, categoria, and saldo, along with a __repr__ method for string representation.

---
+++ src/models/entities/pessoa_juridica.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, BIGINT, REAL
+from src.models.settings.base import Base
+
+
+class PessoaJuridica(Base):
+    __tablename__ = "pessoa_juridica"
+
+    id = Column(BIGINT, primary_key=True)
+    faturamento = Column(REAL)
+    idade = Column(BIGINT)
+    nome_fantasia = Column(String)
+    celular = Column(String)
+    email_corporativo = Column(String)
+    categoria = Column(String)
+    saldo = Column(REAL)
+
+    def __repr__(self) -> str:
+        return (
+            f"PessoaJuridica(id={self.id}, faturamento={self.faturamento}, "
+            f"idade={self.idade}, nome_fantasia={self.nome_fantasia}, "
+            f"celular={self.celular}, email_corporativo={self.email_corporativo}, "
+        )
