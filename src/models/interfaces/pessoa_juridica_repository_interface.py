d4fb3b1c5fcd124d990c1d8760737cc03511dfae
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-27 21:02:50 -0300

    feat: add PessoaJuridicaRepositoryInterface for managing legal entities
    This commit introduces a new interface `PessoaJuridicaRepositoryInterface` to define the contract for inserting and retrieving legal entity data. The interface ensures consistent behavior across implementations.

---
+++ src/models/interfaces/pessoa_juridica_repository_interface.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from src.models.entities.pessoa_juridica import PessoaJuridicaTable
+
+
+class PessoaJuridicaRepositoryInterface(ABC):
+    @abstractmethod
+    def insert_pessoa_juridica(
+        self,
+        faturamento: float,
+        idade: int,
+        nome_fantasia: str,
+        celular: str,
+        email_corporativo: str,
+        categoria: str,
+        saldo: float,
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def get_pessoa_juridica(self, pessoa_id: str) -> PessoaJuridicaTable:
+        pass
